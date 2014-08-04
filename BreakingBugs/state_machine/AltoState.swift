f462e4f64a6c5bbd7d543626fddf5df573560c10
Chad Eubanks <ceubanks@thecodeboutique.com>
2014-08-04 12:41:04 -0700

    - Adds Alto state_machine framework - Updates app to load in Landscape mode

---
+++ BreakingBugs/state_machine/AltoState.swift
@@ -0,0 +1,23 @@
+//
+//  AltoState.swift
+//  BreakingBugs
+//
+//  Created by chad eubanks on 7/28/14.
+//  Copyright (c) 2014 The Code Boutique. All rights reserved.
+//
+
+import Foundation
+
+class AltoState: NSObject {
+    
+    var applicationDelegate:AppDelegate?
+    
+    func enterState() {
+        
+    }
+    
+    func exitState() {
+        
+    }
+
+}
---
+++ BreakingBugs/state_machine/AltoStatechart.swift
@@ -0,0 +1,54 @@
+//
+//  AltoStatechart.swift
+//  BreakingBugs
+//
+//  Created by chad eubanks on 7/28/14.
+//  Copyright (c) 2014 The Code Boutique. All rights reserved.
+//
+
+import Foundation
+
+class AltoStatechart {
+    class var sharedStateManager: AltoStatechart {
+        
+    struct Static {
+        static var instance: AltoStatechart?
+        static var token: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.token) {
+            Static.instance = AltoStatechart()
+        }
+        
+        return Static.instance!
+    }
+    
+    var currentState: AltoState?
+    
+    func setCurrentstate(state:AltoState) {
+        currentState = state
+    }
+    
+    func goToState(state:AltoState) {
+        
+        // if we have a current state... call its exit method
+        if (currentState) {
+            currentState?.exitState()
+        }
+        
+        // update our currentState to the state we are going to
+        currentState = state
+        
+        // enter our new state
+        state.enterState()
+    }
+    
+    func sendEvent (selector:Selector) {
+        // make sure the currentState responds the the given selector
+        if (currentState?.respondsToSelector(selector)) {
+            currentState?.forwardingTargetForSelector(selector)
+        } else {
+        
+        }
+    }
+}
